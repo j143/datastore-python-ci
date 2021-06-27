db8c88fba62748ce0559b80025a3f9e405dcbcce
Janardhan Pulivarthi <j143@protonmail.com>
2021-06-27 17:42:12 +0000

    task update, delete and query

--- tasks.py
+++ tasks.py
@@ -26,5 +26,41 @@ def add_task(client: datastore.Client, description: str):
         }
     )
     client.put(task)
-    
+
     return task.key
+
+# A Method to update `done` property, to indicate the task is complete.
+
+def mark_done(client: datastore.Client, task_id: Union[str, int]):
+    with client.transaction():
+        # Create a key for an entity of kind "Task", this is an immutable
+        # representation
+        key = client.key("Task", task_id)
+        # Load entity with the key
+        task = client.get(key)
+
+        if not task:
+            raise ValueError(f"Task {task_id} does not exist.")
+        
+        # Work done
+        task["done"] = True
+
+        # Persist the change back to Datastore
+        client.put(task)
+
+
+# Now delete `Task` entity, using the `Task` entity's key
+
+def delete_task(client: datastore.Client, task_id: Union[str, int]):
+    key = client.key("Task", task_id)
+    # use the above key to delete associated document, if it exists
+    client.delete(key)
+
+# The query can also be done with 
+# https://cloud.google.com/datastore/docs/reference/gql_reference
+def list_tasks(client: datastore.Client):
+    query = client.query(kind="Task")
+    query.order = ["created"]
+
+    return list(query.fetch())
+
